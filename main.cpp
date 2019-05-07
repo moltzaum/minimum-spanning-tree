eb98c433a3b236adc9693b799c7879ee6ee0461a
moltzaum <matthew@moltzau.net>
2019-05-07 17:22:11 -0600

    Fixed union-find algorithm

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ void handle_error(const char* str) {
 struct Edge;
 
 typedef int Node;
-typedef int* Subtree;
 typedef std::forward_list<Edge> Bucket;
 typedef Edge Graph[5000];
 
@@ -21,11 +20,40 @@ struct Edge {
     Node N1;
     Node N2;
     int weight;
-    Subtree subtree;
+    int subtree;
 };
 
-// node_subtree[n] yeilds a subtree for node 'n'
-Subtree node_subtree[101] = {};
+// node_subtree[node] yeilds the initial subtree for a node
+// the returned result is probably stale. See follow_tree
+int node_subtree[101] = {};
+
+// initially, follow_tree[n] = n
+// After some merges, follow_tree[n] = ?
+// the returned result MAY be stale, so iterate until
+// follow_tree[n] = n
+int follow_tree[50] = {};
+
+void merge_subtree(int s1, int s2) {
+    follow_tree[s1] = follow_tree[s2];
+}
+
+// before lookup_subtree is used, node_subtree should be initialized
+// in parallel to follow_tree. This is so that '0' means uninitialized
+// instead of indicating the original subtree was valid, or the lookup
+// did not need to be looped.
+int lookup_subtree(Node n) {
+    int subtree;
+    int result = node_subtree[n];
+    do {
+        subtree = result;
+        result = follow_tree[subtree];
+    } while (result != subtree);
+
+    // update cached result
+    node_subtree[n] = result;
+    
+    return result;
+}
 
 namespace WeightIterator {
     int num_weights = 0;
@@ -39,15 +67,9 @@ inline void skip_lines(std::istream& in, size_t n) {
         in.ignore(32, '\n');
 }
 
-inline Subtree mk_subtree(int n) {
-    Subtree subtree = new int;
-    *subtree = n;
-    return subtree;
-}
-
 void print_mst(Graph mst, int edges) {
     for (int i = 0; i < edges; i++) {
-        printf("(%d, %d)\n", mst[i].N1, mst[i].N2);
+        //printf("(%d, %d)\n", mst[i].N1, mst[i].N2);
         //printf("(%d, %d) = %d\n", mst[i].N1, mst[i].N2, mst[i].weight);
     }
 }
@@ -58,43 +80,39 @@ int get_mst(Graph mst, int &edges) {
     using namespace WeightIterator;
     
     int cost = 0;
-    int auto_inc = 0;
     int num_edges = 0;
+    int next_subtree = 1;
     Edge e = {};
     
     for (int i = 0; i < num_weights; i++) {
         auto b = buckets[i];
         for (auto it = b.begin(); it != b.end(); ++it) {
             e = *it;
-            Subtree s1 = node_subtree[e.N1];
-            Subtree s2 = node_subtree[e.N2];
             
-            if (*s1 == -1 && *s2 == -1) {
-                //printf("make new subtree %d\n", auto_inc);
-                Subtree s3 = mk_subtree(auto_inc++);
-                node_subtree[e.N1] = s3;
-                node_subtree[e.N2] = s3;
+            int s1 = lookup_subtree(e.N1);
+            int s2 = lookup_subtree(e.N2);
+            
+            if (s1 == 0 && s2 == 0) {
+                node_subtree[e.N1] = next_subtree;
+                node_subtree[e.N2] = next_subtree;
+                follow_tree[next_subtree] = next_subtree;
+                next_subtree++;
                 
-            } else if (*s1 == -1) {
-                //printf("assign s2 subtree to s1 (whole thing)\n");
-                node_subtree[e.N1] = node_subtree[e.N2];
+            } else if (s1 == 0) {
+                node_subtree[e.N1] = s2;
                 
-            } else if (*s2 == -1) {
-                //printf("assign s1 subtree to s2 (whole thing)\n");
-                node_subtree[e.N2] = node_subtree[e.N1];
+            } else if (s2 == 0) {
+                node_subtree[e.N2] = s1;
                 
-            } else if (*s1 == *s2) {
+            } else if (s1 == s2) {
                 // adding edge will make a cycle
-                //printf("skip!\n");
                 continue;
             } else {
-                //printf("assign s1 subtree to s2 (just value)\n");
-                *node_subtree[e.N2] = *node_subtree[e.N1];
+                merge_subtree(s1, s2);
             }
-            
+
             mst[num_edges++] = e;
             cost += e.weight;
-            //printf("{%d, %d} = %d\n", e.N1, e.N2, e.weight);
         }
     }
 
@@ -115,7 +133,6 @@ int ld_weights(char const *fname) {
     in >> nodes;
     lines = nodes * nodes;
      
-    Subtree s0 = mk_subtree(-1);
     Edge e = {};
     
     for (int k = 0; k < lines; k++) {
@@ -141,8 +158,6 @@ int ld_weights(char const *fname) {
         
         num_weights = (num_weights < weight ? weight : num_weights);
         e = {i, j, weight};
-        node_subtree[i] = s0;
-        node_subtree[j] = s0;
         buckets[weight].push_front(e);
     }
     in.close();
@@ -176,6 +191,6 @@ int main(int argc, char*argv[]) {
     print_mst(mst, edges);
     
     // 1) What would it take to print the MST like how the example gives it?
-    // 2) I should delete dynamically-allocated memory
-    // 5) I should get rid of global variables
+    // 2) I should get rid of global variables
+    // 3) I should organize files
 }
